188e72415cfa1c79acba60cbb395ce78d3d3aa1a
Brandt Kurowski <bkurowski@uceap.universityofcalifornia.edu>
2025-05-08 05:36:56 -0400

    extract reusable bit

--- local/etc/uceap.d/_pantheon_deploy.sh
+++ local/etc/uceap.d/_pantheon_deploy.sh
@@ -7,6 +7,10 @@ function _pantheon_deploy() {
 	terminus drush -- state-set system.maintenance_mode TRUE
 	terminus env:clear-cache
 	terminus env:deploy $deploy_args
+	_pantheon_complete_deployment
+}
+
+function _pantheon_complete_deployment() {
 	terminus drush -- cache-rebuild
 	terminus drush -- $DRUSH_TASK
 	sleep 60
