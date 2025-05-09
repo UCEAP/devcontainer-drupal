ff9d659a4ac1a4ebe612ea9fe2ee74777e069d48
Brandt Kurowski <bkurowski@uceap.universityofcalifornia.edu>
2025-05-09 16:16:11 -0400

    troubleshooting deployment issues

--- local/etc/uceap.d/deploy-to-dev.sh
+++ local/etc/uceap.d/deploy-to-dev.sh
@@ -3,6 +3,7 @@ function deploy_to_dev() {
 	_deploy_code
 	sleep 60
 	terminus env:commit --force -y
+	terminus connection:set git -y
 }
 
 _deploy_to_dev_desc='deploys the local code to the Pantheon DEV environment'
