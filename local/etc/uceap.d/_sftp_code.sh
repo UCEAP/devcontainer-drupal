4ee4ba01fba82893fa0f867c1003492219665322
Brandt Kurowski <bkurowski@uceap.universityofcalifornia.edu>
2025-05-12 14:41:59 -0400

    🤦

--- local/etc/uceap.d/_sftp_code.sh
+++ local/etc/uceap.d/_sftp_code.sh
@@ -4,5 +4,5 @@ function _sftp_code() {
 	SFTP_HOST=`terminus connection:info --field=sftp_host`
 
 	terminus connection:set sftp -y
-	rsync -e "ssh -p $SFTP_PORT -o StrictHostKeyChecking=no" -rv --checksum --delete --exclude=web/sites/default/files ./ $SFTP_USER@$SFTP_HOST:code/
+	rsync -e "ssh -p $SFTP_PORT -o StrictHostKeyChecking=no" -rv --checksum --delete --exclude=.git --exclude=web/sites/default/files ./ $SFTP_USER@$SFTP_HOST:code/
 }
