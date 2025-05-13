efd03e826e464eb568f3a2cc3016e5f1f672029f
Brandt Kurowski <bkurowski@uceap.universityofcalifornia.edu>
2025-05-13 17:56:13 -0400

    workaround platform delay

--- local/etc/uceap.d/deploy-to-dev.sh
+++ local/etc/uceap.d/deploy-to-dev.sh
@@ -2,6 +2,7 @@ function deploy_to_dev() {
 	export TERMINUS_ENV="dev"
 	_terminus_login
 	_deploy_code
+	sleep 60 # code sync sometimes takes a while on the distributed filesystem
 	terminus env:commit --force -y
 }
 
